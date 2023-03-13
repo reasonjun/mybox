e91203e46a5625e932fc845179690c979152113a
seongjun <reasonjun@daum.net>
2023-03-13 23:47:50 +0900

    initial commit

---
+++ src/main/java/com/reasonjun/mybox/MyboxApplication.java
@@ -0,0 +1,13 @@
+package com.reasonjun.mybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reasonjun/mybox/MyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reasonjun.mybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
